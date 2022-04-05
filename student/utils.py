23d414c4a8062d93cc128086b17d660ae7011d7a
quinten <cmoiquinten@hotmail.fr>
2022-04-05 18:02:10 +0200

    better distance and missing ,

--- student/utils.py
+++ student/utils.py
@@ -1,6 +1,8 @@
 """
 Utilitaires utilises par differents programmes
 """
+from random import random
+from geo.point import Point
 
 def print_answer(closest):
     """
@@ -10,7 +12,7 @@ def print_answer(closest):
     p1_coord = closest[0].coordinates
     p2_coord = closest[1].coordinates
 
-    print(f"{p1_coord[0]}, {p1_coord[1]}; {p2_coord[0]} {p2_coord[1]}")
+    print(f"{p1_coord[0]}, {p1_coord[1]}; {p2_coord[0]}, {p2_coord[1]}")
 
 def generate_point_sample(number):
     """
@@ -27,3 +29,15 @@ def print_array(l):
     """
     for i in range(len(l)):
         print(f"{i:4} : {l[i]}")
+
+def distance2(point1, point2):
+    """
+    Renvoie le carre de la distance euclidienne
+    C'est plus performant, et equivalent. On peut
+    cependant aussi utiliser une autre distance,
+    vu que le sujet ne precise rien a ce sujet.
+    """
+    d2  = (point1.coordinates[0] - point2.coordinates[0]) ** 2
+    d2 += (point1.coordinates[1] - point2.coordinates[1]) ** 2
+
+    return d2
